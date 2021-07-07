52348f901cd999f5d332bade019159f2208fd1a0
mbanon <mbanon@prompsit.com>
2021-07-07 14:13:00 +0000

    FastSpell callable as a module

---
+++ FastSpell.py
@@ -0,0 +1,177 @@
+from sacremoses import MosesTokenizer
+import fasttext
+import hunspell
+import logging
+
+
+
+#Removes punctuation and propernouns to avoid 
+#Hunspell error rates too high
+#and focus only on "normal" words.
+def remove_non_alpha_and_propernouns(tokens):
+    newtokens = []
+    isfirsttoken=True
+    for token in tokens:
+        if token.upper() != token.lower() and (isfirsttoken or token[0]!=token[0].upper()):    
+            newtokens.append(token.lower())
+        isfirsttoken=False    
+    return newtokens        
+
+class FastSpell:
+    
+    threshold = 0.25 #Hunspell max error rate allowed in a sentence
+    dictpath="/usr/share/hunspell/" #Hunspell .dic and .aff files directory
+    prefix = "__label__" #FastText returns langs labeled as __label__LANGCODE
+    
+    #Target langs (keys) dict for mistakeable languages (values)
+    similar_langs = {
+    "ca":["es", "ca"],
+    "da":["da", "nb"],
+    "es":["es", "gl", "ca"],
+    "gl":["es", "pt", "gl"],
+    "nb":["nn", "da", "nb"],
+    "nn":["nb", "da", "nn"],
+    "bs":["hr", "sr", "me", "mk", "sq", "bs"],
+    "cs":["sk", "cs"],
+    "sk":["cs", "sk"],
+    "hr":["bs", "sr", "me", "mk", "sq", "hr"],
+    "me":["bs", "hr", "mk", "sr", "sq", "me"],
+    "mk":["bs", "hr", "me", "sr", "sq", "mk"],
+    "sq":["bs", "hr", "me", "mk", "sr", "sq"],
+    "sr":["bs", "hr", "me", "mk", "sq", "sr"]
+    }
+
+
+    #TO DO: Change to anything more maintainable
+    tokenizers = {
+    "ca": MosesTokenizer("ca"),
+    "gl": MosesTokenizer("es"), #TO DO
+    "nb": MosesTokenizer("nb"),
+    "nn": MosesTokenizer("nb"), #TO DO
+    "da": MosesTokenizer("da"),
+    "bs": MosesTokenizer("en"), #TO DO
+    "cs": MosesTokenizer("cs"),
+    "sk": MosesTokenizer("sk"),
+    "hr": MosesTokenizer("en"), #TO DO
+    "me": MosesTokenizer("en"), #TO DO
+    "mk": MosesTokenizer("en"), #TO DO
+    "sq": MosesTokenizer("en"), #TO DO
+    "sr": MosesTokenizer("en"), #TO DO
+    "es": MosesTokenizer("es"),
+    "pt": MosesTokenizer("pt")
+    }
+
+
+    #This is how hunspell files (.dic and .aff) are named in dictpath
+    hunspell_codes = {
+    "ca": "ca_ES",
+    "gl": "gl_ES",
+    "nb": "nb_NO",
+    "nn": "nn_NO",
+    "da": "da_DK",
+    "bs": "bs_BA",
+    "cs": "cs_CZ",
+    "sk": "sk_SK",
+    "hr": "hr_HR",
+    "me": "sr_ME",
+    "mk": "mk_MK",
+    "sq": "sq_AL",
+    "sr": "sr_Latn_RS",
+    "es": "es_ES",
+    "pt": "pt_PT"
+    }
+
+    hunspell_objs = {}
+
+    def __init__(self, lang, mode="cons"):
+        assert (mode=="cons" or mode=="aggr"), "Unknown mode. Use 'aggr' for aggressive or 'cons' for conservative"
+
+        self.lang = lang
+        self.mode = mode
+
+        try:
+            self.model = fasttext.load_model('lid.176.bin')  #FastText model
+        except ValueError as ex:
+            logging.warning("Downloading FastText model...")
+            urllib.request.urlretrieve("https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin", "lid.176.bin")
+            self.model = fasttext.load_model('lid.176.bin') 
+
+        self.similar = self.similar_langs.get(lang)
+        #If there are languages that can be mistaken 
+        #with the target language: prepare an array of Hunspell spellcheckers
+        #for all the similar languages
+        if self.similar != None:
+            for l in self.similar:
+                dict = self.dictpath+self.hunspell_codes.get(l)
+                hunspell_obj = hunspell.HunSpell(dict+'.dic', dict+'.aff') 
+                self.hunspell_objs[l] = hunspell_obj
+
+
+    def getlang(self, sent):
+        sent=sent.strip()
+        prediction = self.model.predict(sent, k=1)[0][0][len(self.prefix):]
+        #classic norwegian ñapa
+        if prediction == "no":
+            prediction = "nb"
+        #TODO: Confidence score?
+
+        if self.similar == None or prediction not in self.similar:
+        #Non mistakeable language: just return FastText prediction
+            return(prediction)
+        else:
+        #The target language is mistakeable
+            spellchecked = {}
+            for l in self.hunspell_objs:
+                #Get spellchecking for all the mistakeable languages
+                logging.debug(l)
+                dec_sent = sent.encode(encoding='UTF-8',errors='strict').decode('UTF-8') #Not 100% sure about this...
+                raw_toks = self.tokenizers.get(l).tokenize(dec_sent, escape=False)
+                toks = remove_non_alpha_and_propernouns(raw_toks)
+                try:
+                    correct_list = list(map(self.hunspell_objs.get(l).spell, toks))
+                except UnicodeEncodeError: #...because it sometimes fails here for certain characters
+                    correct_list = []
+                corrects = sum(correct_list*1)
+                logging.debug("Tokens: " +str(toks))
+                logging.debug("Corrects: " + str(correct_list))
+                logging.debug("Total: " + str(len(toks)))
+                if corrects > 0:
+                    error_rate = 1-(corrects/len(toks))
+                else:
+                    error_rate = 1
+                logging.debug("error_rate: " + str(error_rate))
+                if error_rate < self.threshold: #we don't keep it if the error rate is above the threshold
+                    spellchecked[l] =  error_rate
+                logging.debug("----------------")
+
+            if len(spellchecked) > 0:
+                #at least one of the spellchecks was below the threshold            
+                #get best values and keys
+                best_value = min(spellchecked.values())
+                best_keys = [k for k, v in spellchecked.items() if v == best_value]
+                if len(best_keys)==1:
+                    #Only one language scoring the best
+                    return(best_keys[0])
+                else:
+                    #It's a tie!
+                    if self.mode == "aggr":
+                        #Aggressive approach: if the targetted language is among the best scoring, take it
+                        if self.lang in best_keys:
+                            return(self.lang)
+                        elif prediction in best_keys:
+                            #the targetted language is not in the best ones, and the prediction?
+                            return(prediction)
+                        else:
+                            #Just take one
+                            return(best_keys[0])
+                    if self.mode == "cons":
+                        #Conservative: just keep it as unknown
+                        return("unk")
+            else:
+                #Nothing in the spellchecking list
+                if self.mode == "aggr":
+                    return(prediction)
+                else:
+                    return("unk")
+
+
--- fastspell.py
+++ fastspell.py
@@ -1,85 +1,18 @@
+import io
 import sys
 import timeit
 import argparse
+import traceback
 import logging
 import os
-import fasttext
-import hunspell
 import urllib.request
-from sacremoses import MosesTokenizer
+import FastSpell
+
 
 __author__ = "Marta Bañón"
 __version__ = "Version 0.1 # 01/07/2021 # Initial release # Marta Bañón"
 
-try:
-    model = fasttext.load_model('lid.176.bin')  #FastText model
-except ValueError as ex:
-    logging.warning("Downloading FastText model...")
-    urllib.request.urlretrieve("https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin", "lid.176.bin")
-    model = fasttext.load_model('lid.176.bin') 
-    
-threshold = 0.25 #Hunspell max error rate allowed in a sentence
-dictpath="/usr/share/hunspell/" #Hunspell .dic and .aff files directory
-prefix = "__label__" #FastText returns langs labeled as __label__LANGCODE
 
-#Target langs (keys) dict for mistakeable languages (values)
-similar_langs = {
-"ca":["es", "ca"],
-"da":["da", "nb"],
-"es":["es", "gl", "ca"],
-"gl":["es", "pt", "gl"],
-"nb":["nn", "da", "nb"],
-"nn":["nb", "da", "nn"],
-"bs":["hr", "sr", "me", "mk", "sq", "bs"],
-"cs":["sk", "cs"],
-"sk":["cs", "sk"],
-"hr":["bs", "sr", "me", "mk", "sq", "hr"],
-"me":["bs", "hr", "mk", "sr", "sq", "me"],
-"mk":["bs", "hr", "me", "sr", "sq", "mk"],
-"sq":["bs", "hr", "me", "mk", "sr", "sq"],
-"sr":["bs", "hr", "me", "mk", "sq", "sr"]
-}
-
-
-#TO DO: Change to anything more maintainable
-tokenizers = {
-"ca": MosesTokenizer("ca"),
-"gl": MosesTokenizer("es"), #TO DO
-"nb": MosesTokenizer("nb"),
-"nn": MosesTokenizer("nb"), #TO DO
-"da": MosesTokenizer("da"),
-"bs": MosesTokenizer("en"), #TO DO
-"cs": MosesTokenizer("cs"),
-"sk": MosesTokenizer("sk"),
-"hr": MosesTokenizer("en"), #TO DO
-"me": MosesTokenizer("en"), #TO DO
-"mk": MosesTokenizer("en"), #TO DO
-"sq": MosesTokenizer("en"), #TO DO
-"sr": MosesTokenizer("en"), #TO DO
-"es": MosesTokenizer("es"),
-"pt": MosesTokenizer("pt")
-}
-
-#This is how hunspell files (.dic and .aff) are named in dictpath
-hunspell_codes = {
-"ca": "ca_ES",
-"gl": "gl_ES",
-"nb": "nb_NO",
-"nn": "nn_NO",
-"da": "da_DK",
-"bs": "bs_BA",
-"cs": "cs_CZ",
-"sk": "sk_SK",
-"hr": "hr_HR",
-"me": "sr_ME",
-"mk": "mk_MK",
-"sq": "sq_AL",
-"sr": "sr_Latn_RS",
-"es": "es_ES",
-"pt": "pt_PT"
-}
-
-hunspell_objs = {}
 
 
 def logging_setup(args = None):
@@ -103,129 +36,65 @@ def logging_setup(args = None):
             logger.setLevel(logging.DEBUG)
             
             
-#Removes punctuation and propernouns to avoid 
-#Hunspell error rates too high
-#and focus only on "normal" words.
-def remove_non_alpha_and_propernouns(tokens):
-    newtokens = []
-    isfirsttoken=True
-    for token in tokens:
-        if token.upper() != token.lower() and (isfirsttoken or token[0]!=token[0].upper()):    
-            newtokens.append(token.lower())
-        isfirsttoken=False    
-    return newtokens        
 
-
-parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
-parser.add_argument('lang', type=str)
-
-parser.add_argument('--aggr', action='store_true', help='Aggressive strategy (more positives)')
-parser.add_argument('--cons', action='store_true',  help='Conservative strategy (less positives)')
-
-groupL = parser.add_argument_group('Logging')
-groupL.add_argument('-q', '--quiet', action='store_true', help='Silent logging mode')
-groupL.add_argument('--debug', action='store_true', help='Debug logging mode')
-groupL.add_argument('--logfile', type=argparse.FileType('a'), default=sys.stderr, help="Store log to a file")
-groupL.add_argument('-v', '--version', action='version', version="%(prog)s " + __version__, help="show version of this script and exit")
+def initialization():
+    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
+    parser.add_argument('lang', type=str)
+    parser.add_argument('input',  nargs='?', type=argparse.FileType('rt', errors="replace"), default=io.TextIOWrapper(sys.stdin.buffer, errors="replace"),  help="Input sentences.")
+    parser.add_argument('output', nargs='?', type=argparse.FileType('wt'), default=sys.stdout, help="Output of the language identification.")
     
-args = parser.parse_args()
-logging_setup(args)
-
-if args.aggr == args.cons:
-    #both are true or both are false
-    logging.error("Please provide  --aggr or --cons")
-    exit(1)
-
-similar = similar_langs.get(args.lang)
-
-#If there are languages that can be mistaken 
-#with the target language: prepare an array of Hunspell spellcheckers
-#for all the similar languages
-if similar != None:
-    for l in similar:
-        dict = dictpath+hunspell_codes.get(l)
-        hunspell_obj = hunspell.HunSpell(dict+'.dic', dict+'.aff') 
-        hunspell_objs[l] = hunspell_obj
+    parser.add_argument('--aggr', action='store_true', help='Aggressive strategy (more positives)')
+    parser.add_argument('--cons', action='store_true',  help='Conservative strategy (less positives)')
+    
+    groupL = parser.add_argument_group('Logging')
+    groupL.add_argument('-q', '--quiet', action='store_true', help='Silent logging mode')
+    groupL.add_argument('--debug', action='store_true', help='Debug logging mode')
+    groupL.add_argument('--logfile', type=argparse.FileType('a'), default=sys.stderr, help="Store log to a file")
+    groupL.add_argument('-v', '--version', action='version', version="%(prog)s " + __version__, help="show version of this script and exit")
         
+    args = parser.parse_args()
+    logging_setup(args)
     
-start_time = timeit.default_timer()
+    if args.aggr == args.cons:
+        #both are true or both are false
+        logging.error("Please provide  --aggr or --cons")
+        exit(1)
+    return args
 
-for line in sys.stdin:
-    parts = line.strip().split("\t")
-    sent = parts[0]
-    prediction = model.predict(sent, k=1)[0][0][len(prefix):]
-    #classic norwegian ñapa
-    if prediction == "no":
-        prediction = "nb"
-    #TODO: Confidence score?
-    
-    if similar == None or prediction not in similar:
-    #Non mistakeable language
-        if prediction == args.lang:
-            #this is ok
-            print(sent+"\t"+prediction)
-        else:
-            #this is not ok (but output anyway because this is a benchmark hehe)
-            print(sent+"\t"+prediction)    
-    else:
-    #The target language is mistakeable
-        spellchecked = {}
-        for l in hunspell_objs:
-            #Get spellchecking for all the mistakeable langauges
-            logging.debug(l)
-            dec_sent = sent.encode(encoding='UTF-8',errors='strict').decode('UTF-8') #Not 100% sure about this...
-            raw_toks = tokenizers.get(l).tokenize(dec_sent, escape=False)
-            toks = remove_non_alpha_and_propernouns(raw_toks)
-            try:
-                correct_list = list(map(hunspell_objs.get(l).spell, toks))                      
-            except UnicodeEncodeError: #...because it sometimes fails here for certain characters
-                correct_list = []    
-            corrects = sum(correct_list*1)
-            logging.debug("Tokens: " +str(toks))           
-            logging.debug("Corrects: " + str(correct_list))
-            logging.debug("Total: " + str(len(toks)))            
-            if corrects > 0:
-                error_rate = 1-(corrects/len(toks))
-            else:
-                error_rate = 1
-            logging.debug("error_rate: " + str(error_rate))
-            if error_rate < threshold: #we don't keep it if the error rate is above the threshold
-                spellchecked[l] =  error_rate
-            logging.debug("----------------")
-            
-        if len(spellchecked) > 0:
-            #at least one of the spellchecks was below the threshold            
-            #get best values and keys
-            best_value = min(spellchecked.values())
-            best_keys = [k for k, v in spellchecked.items() if v == best_value]
-            if len(best_keys)==1:
-                #Only one language scoring the best
-                print(sent+"\t"+best_keys[0])
-            else:
-                #It's a tie!
-                if args.aggr:
-                    #Aggressive approach: if the targetted language is among the best scoring, take it
-                    if args.lang in best_keys:
-                        print(sent+"\t"+args.lang)
-                    elif prediction in best_keys:
-                        #the targetted language is not in the best ones, and the prediction?
-                        print(sent+"\t"+prediction)
-                    else:
-                        #Just take one
-                        print(sent+"\t"+best_keys[0])
-                if args.cons:
-                    #Conservative: just keep it as unknown
-                    print(sent+"\t"+"unk")            
-                                         
-        else:
-            #Nothing in the spellchecking list
-            if args.aggr:
-                print(sent+"\t"+prediction)
-            else:    
-                print(sent+"\t"+"unk")
-                
-end_time = timeit.default_timer()
+
+
+
+def perform_identification(args):
+
+    time_start = timeit.default_timer()
+    if args.aggr:
+        mode="aggr"
+    if args.cons:
+        mode="cons"
         
-
-logging.info("Elapsed time: {}".format(end_time - start_time))
+    fs = FastSpell.FastSpell(args.lang, mode=mode)
     
+    for line in args.input:        
+        lident = fs.getlang(line)
+        args.output.write(line.strip()+"\t"+lident+"\n")
+        
+    end_time = timeit.default_timer()
+    logging.info("Elapsed time: {}".format(end_time - time_start))     
+         
+
+
+
+def main(args):
+    logging.info("Executing main program...")
+    perform_identification(args)
+    logging.info("Program finished")
+
+if __name__ == '__main__':
+    try:
+        logging_setup()
+        args = initialization() # Parsing parameters
+        main(args)  # Running main program
+    except Exception as ex:
+        tb = traceback.format_exc()
+        logging.error(tb)
+        sys.exit(1)
